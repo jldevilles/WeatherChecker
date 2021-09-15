210b6390290b0d915adb1d17213ce4c33ebbebd5
jldevilles <jldevilles@gmail.com>
2021-09-15 09:20:05 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace WeatherChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            IConfiguration Config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string sAccess_Key = Config.GetSection("Rules").GetSection("Access_Key").Value;
+            string sURL = Config.GetSection("URL").Value;
+            int iWind = Convert.ToInt32(Config.GetSection("Wind_Speed_Min").Value);
+            int iUVMax = Convert.ToInt32(Config.GetSection("UV_Max_Threshold").Value);
+            bool bIsRaining = false;
+            string[] sRainCodes = Config.GetSection("Rules").GetSection("Rain_Codes").Get<string[]>();
+
+            Console.Write("Enter Your ZIP Code and Press Any Key:");
+            var zip = Console.ReadLine();
+            int X;
+            WeatherStack WS = new WeatherStack();
+            WS.AccessKey = sAccess_Key;
+            WS.URL = sURL;
+
+            while (!Int32.TryParse(zip, out X)) // Only Accept Number for Zip Code
+            {
+                Console.WriteLine("Not a valid ZIP CODE, Please Try Again.");
+
+                zip = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+            WeatherResults WSResults = new WeatherResults();
+            WSResults = WS.PerformWeatherCheck(zip);
+
+            if (WSResults.Status == "success")
+            {
+                Console.WriteLine("YOUR LOCATION IS: " + WSResults.Location);
+                Console.WriteLine();
+
+
+                //Check if it's raining
+                string sWeatherCode = WSResults.WeatherCode;
+                int stringIndex = Array.IndexOf(sRainCodes, sWeatherCode); // Loop through rain weather codes to check all rain codes
+                if (stringIndex >= 0)
+                {
+                    Console.WriteLine("Should I go outside?: No");  // If string is found then it's raining don't go out
+                    bIsRaining = true;  // It's raining set to true
+                }
+                else
+                {
+                    Console.WriteLine("Should I go outside?: Yes! | The weather is: " + WSResults.WeatherDescription[0]); ;
+                    bIsRaining = false; // It's NOT raining set to flase
+                }
+
+                //Check if it's hot
+                if (WSResults.UV_Index > iUVMax) // UV Allowable rate is 3, more than the wear sunscreen
+                {
+                    Console.WriteLine("Should I wear sunscreen?: Yes | UV INDEX IS: " + WSResults.UV_Index);
+                }
+                else
+                {
+                    Console.WriteLine("Should I wear sunscreen?: No | UV INDEX IS: " + WSResults.UV_Index);
+                }
+
+                if (WSResults.WindSpeed > iWind) //Windspeed should be more than 15 value to fly a kite
+                {
+                    if (bIsRaining) // don't fly if its raining
+                    {
+                        Console.WriteLine("Can I fly my kite?: No | Wind Speed is: " + WSResults.WindSpeed);
+                        Console.WriteLine("It's windy but it's also raining");
+                    }
+                    else
+                        Console.WriteLine("Can I fly my kite?: Yes! | Wind Speed is: " + WSResults.WindSpeed);
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Can I fly my kite?: No | Wind Speed is: " + WSResults.WindSpeed);
+                    Console.WriteLine("There's not enough wind speed to fly your kite.");
+
+                }
+
+
+
+                // var currentDate = DateTime.Now;
+                //  Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+                Console.Write($"{Environment.NewLine}Press any key to exit...");
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine("The application is having difficulty accessing WeatherStack API");
+                Console.WriteLine("Please contact the author or try again later.");
+                Console.WriteLine("Error Message: " + WSResults.ErrorMsg); 
+                Console.Write($"{Environment.NewLine}Press any key to exit...");
+            }
+        }
+    }
+}
---
+++ WeatherStack.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+namespace WeatherChecker
+{
+    class WeatherStack
+    {
+        #region -- Properties --
+
+            private string _AccessKey = "";
+            private string _URL = "";
+
+        #endregion
+
+        public WeatherResults PerformWeatherCheck(string sZipCode)
+        {
+            WeatherResults WRData = new();
+            
+            try
+            {
+                                
+                string destinationUrl = _URL + "current?access_key=" + _AccessKey + "&query=" + sZipCode;
+
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(destinationUrl);  // Access WeatherStack API to fetch the weather data by Zip Code
+                httpWebRequest.ProtocolVersion = HttpVersion.Version11; 
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+
+                    string json = JsonConvert.SerializeObject(new{});
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                HttpWebResponse response;
+                response = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                Stream newStream = response.GetResponseStream();
+                StreamReader sr = new StreamReader(newStream);
+                var result = sr.ReadToEnd();
+                
+                var splashInfo = JsonConvert.DeserializeObject<TransactionResult>(result);  // Parse WeatherStack Weather Data and Place it to Weather Data Class
+                if(splashInfo.Location == null)
+                {
+                    throw new InvalidOperationException("API Error or ZIPCODE not Found");
+                }
+                else
+                { 
+                    WRData.Location = splashInfo.Location.Name + ", " + splashInfo.Location.Country;
+                    WRData.WeatherCode = splashInfo.Current.Weather_Code;
+                    WRData.WeatherDescription = splashInfo.Current.Weather_Descriptions;
+                    WRData.WindSpeed = Convert.ToInt32(splashInfo.Current.Wind_Speed);
+                    WRData.UV_Index = Convert.ToInt32(splashInfo.Current.UV_Index);
+                }
+
+
+
+            }
+            catch (Exception x)
+            {
+                WRData.Status = "fail";
+                WRData.ErrorMsg = x.Message;
+
+            }
+
+
+            return WRData;
+
+
+
+        }
+
+        public string AccessKey
+        {
+            get { return _AccessKey; }
+            set { _AccessKey = value; }
+        }
+
+        public string URL
+        {
+            get { return _URL; }
+            set { _URL = value; }
+        }
+    }
+
+
+}
+    public class TransactionResult
+    {
+
+        public string Sucess { get; set; }
+        public Location Location{ get; set; }
+        public Current Current { get; set; }
+
+     }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Localtime { get; set; }
+        
+  
+    }
+
+    public class Current
+    {
+            public string Weather_Code { get; set; }
+            public IList<string> Weather_Descriptions { get; set; }        
+            public string Temperature { get; set; }
+
+            public int Wind_Speed { get; set; }
+
+            public int UV_Index { get; set; }
+            
+}
+
+
+
+    public class WeatherResults
+    {
+
+            private string _APISuccess = "true";
+            private string _Status = "success";
+            private string _ErrorMsg = "";
+            private string _Location = "";
+            private string _WeatherCode = "";
+            private int _WindSpeed = 0;
+            private int _UV_Index = 0;
+            private IList<string> _WeatherDescription;
+
+            public string APISuccess
+            {
+                get { return _APISuccess; }
+                set { _APISuccess = value; }
+            }
+            public string Status
+            {
+                get { return _Status; }
+                set { _Status = value; }
+            }
+
+            public string ErrorMsg
+            {
+                get { return _ErrorMsg; }
+                set { _ErrorMsg = value; }
+            }
+            public string Location
+            {
+                get { return _Location; }
+                set { _Location = value; }
+            }
+
+            public string WeatherCode
+            {
+                get { return _WeatherCode; }
+                set { _WeatherCode = value; }
+            }
+
+            public IList<string> WeatherDescription
+            {
+                get { return _WeatherDescription; }
+                set { _WeatherDescription = value; }
+            }
+
+            public int WindSpeed
+            {
+                get { return _WindSpeed; }
+                set { _WindSpeed = value; }
+            }
+
+            public int UV_Index
+            {
+                get { return _UV_Index; }
+                set { _UV_Index = value; }
+            }
+
+
+
+
+
+
+
+
+}
+
